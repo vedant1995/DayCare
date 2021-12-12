c73a15e64136b1d1cbf204455de58b9e7403e154
Vedant Deokar <59900424+vedant1995@users.noreply.github.com>
2021-12-11 21:40:07 -0500

    create a registrationPanel

--- src/main/java/View/DashboardPanel.java
+++ src/main/java/View/DashboardPanel.java
@@ -243,6 +243,11 @@ public class DashboardPanel extends javax.swing.JPanel {
         registrationButton.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
         registrationButton.setText("Registration");
         registrationButton.setToolTipText("");
+        registrationButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                registrationButtonActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout jPanel7Layout = new javax.swing.GroupLayout(jPanel7);
         jPanel7.setLayout(jPanel7Layout);
@@ -321,6 +326,10 @@ public class DashboardPanel extends javax.swing.JPanel {
         dashboardContainer.add(tp);
         layout.next(dashboardContainer);
     }//GEN-LAST:event_teachersButtonActionPerformed
+
+    private void registrationButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_registrationButtonActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_registrationButtonActionPerformed
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
