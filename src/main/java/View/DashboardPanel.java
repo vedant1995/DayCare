4ef65246a5fb23aaf413f65540d2d0c2ffefe606
Vedant Deokar <59900424+vedant1995@users.noreply.github.com>
2021-12-11 19:04:19 -0500

    navigate to teachers panel

--- src/main/java/View/DashboardPanel.java
+++ src/main/java/View/DashboardPanel.java
@@ -9,6 +9,7 @@ import View.Students.StudentsPanel;
 import Controller.ImageHelper;
 import Model.Admin;
 import Model.School;
+import View.Teachers.TeachersPanel;
 import java.awt.CardLayout;
 import java.awt.Color;
 import java.awt.Image;
@@ -315,9 +316,9 @@ public class DashboardPanel extends javax.swing.JPanel {
 
     private void teachersButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_teachersButtonActionPerformed
         // TODO add your handling code here:
-        StudentsPanel dp = new StudentsPanel(dashboardContainer, school);
+        TeachersPanel tp = new TeachersPanel(dashboardContainer, school);
         CardLayout layout = (CardLayout) dashboardContainer.getLayout();
-        dashboardContainer.add(dp);
+        dashboardContainer.add(tp);
         layout.next(dashboardContainer);
     }//GEN-LAST:event_teachersButtonActionPerformed
 
