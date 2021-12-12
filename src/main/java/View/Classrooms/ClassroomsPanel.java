24313ec4f4ec220968eb0b447a20a88d2018079e
Vedant Deokar <59900424+vedant1995@users.noreply.github.com>
2021-12-12 04:39:16 -0500

    add classrooms panel and table

--- src/main/java/View/Classrooms/ClassroomsPanel.java
+++ src/main/java/View/Classrooms/ClassroomsPanel.java
@@ -5,19 +5,21 @@
  */
 package View.Classrooms;
 
+import View.Teachers.*;
+import View.Students.*;
+import Model.AbstractPerson;
 import Model.Classroom;
 import Model.School;
 import Model.Student;
 import Model.Teacher;
+import java.awt.CardLayout;
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Map;
 import javax.swing.JPanel;
-import javax.swing.JTree;
-import javax.swing.tree.DefaultMutableTreeNode;
-import javax.swing.tree.DefaultTreeModel;
-import javax.swing.tree.TreePath;
+import javax.swing.table.DefaultTableModel;
 
 /**
  *
@@ -29,69 +31,34 @@ public class ClassroomsPanel extends javax.swing.JPanel {
     private School school;
 
     /**
-     * Creates new form NewJPanel
+     * Creates new form StudentsPanel
      */
     public ClassroomsPanel(JPanel container, School school) {
         initComponents();
         this.container = container;
         this.school = school;
-        
-        // populateTree();
+        // school.showAll();
+        populateTable();
     }
 
-    /*
-    public void populateTree() {
-        DefaultTreeModel model = (DefaultTreeModel) schoolTree.getModel();
-        List<Classroom> classroomList = school.getClassroomList();
-        List<Student> studentsList;
-        List<Teacher> teachersList;
-
-        Classroom classroom;
-        Teacher teacher;
-        Student student;
-
-        DefaultMutableTreeNode classrooms = new DefaultMutableTreeNode("Classrooms");
-        DefaultMutableTreeNode root = (DefaultMutableTreeNode) model.getRoot();
-        root.removeAllChildren();
-        root.insert(classrooms, 0);
-
-        DefaultMutableTreeNode classroomNode;
-        DefaultMutableTreeNode teacherNode;
-        DefaultMutableTreeNode studentNode;
-
-        // Iterator itr = new Iterator(networkList);
-        for (int i = 0; i < classroomList.size(); i++) {
-            classroom = classroomList.get(i);
-            classroomNode = new DefaultMutableTreeNode("ClassroomId - " + classroom.getId());
-            classrooms.insert(classroomNode, i);
-
-            teachersList = classroom.getTeacherList()
-                    .stream()
-                    .map(person -> (Teacher) person)
-                    .collect(Collectors.toList());
-
-            for (int j = 0; j < teachersList.size(); j++) {
-                teacher = teachersList.get(j);
-                teacherNode = new DefaultMutableTreeNode("Teacher: " + teacher.getFirstName());
-                classroomNode.insert(teacherNode, j);
-
-                studentsList = teacher.getStudentList()
-                        .stream()
-                        .map(person -> (Student) person)
-                        .collect(Collectors.toList());
-
-                for (int k = 0; k < studentsList.size(); k++) {
-                    //System.out.println("i is "+i+ "  org size "+organizationList.size());
-                    student = studentsList.get(k);
-                    studentNode = new DefaultMutableTreeNode(student.getFirstName() + " " + student.getLastName());
-                    teacherNode.insert(studentNode, k);
-                }
-            }
-        }        
-        model.reload();
+    public void populateTable() {
+        DefaultTableModel model = (DefaultTableModel) classroomsTable.getModel();
+        model.setRowCount(0);
+        school.showAll();
+        // TODO: Add wage column to the table
+        for (Classroom classroom
+                : school.getClassroomList()) {
+            Object[] row = new Object[4];
+            row[0] = classroom.getId();
+            int low = classroom.getAgeRange()[0];
+            int high = classroom.getAgeRange()[0];
+            String range = String.valueOf(low) + " - " + String.valueOf(high);
+            row[1] = range;
+            row[2] = classroom.getCount();
+            row[3] = classroom.getSize();
+            model.addRow(row);
+        }
     }
-*/ 
-
 
     /**
      * This method is called from within the constructor to initialize the form.
@@ -102,38 +69,140 @@ public class ClassroomsPanel extends javax.swing.JPanel {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jPanel2 = new javax.swing.JPanel();
+        backButton = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
         jScrollPane1 = new javax.swing.JScrollPane();
-        schoolTree = new javax.swing.JTree();
+        classroomsTable = new javax.swing.JTable();
+        jPanel1 = new javax.swing.JPanel();
+        viewDetailsButton = new javax.swing.JButton();
 
-        javax.swing.tree.DefaultMutableTreeNode treeNode1 = new javax.swing.tree.DefaultMutableTreeNode("School");
-        javax.swing.tree.DefaultMutableTreeNode treeNode2 = new javax.swing.tree.DefaultMutableTreeNode("Classroom");
-        javax.swing.tree.DefaultMutableTreeNode treeNode3 = new javax.swing.tree.DefaultMutableTreeNode("Teacher");
-        javax.swing.tree.DefaultMutableTreeNode treeNode4 = new javax.swing.tree.DefaultMutableTreeNode("red");
-        treeNode3.add(treeNode4);
-        treeNode4 = new javax.swing.tree.DefaultMutableTreeNode("yellow");
-        treeNode3.add(treeNode4);
-        treeNode2.add(treeNode3);
-        treeNode1.add(treeNode2);
-        schoolTree.setModel(new javax.swing.tree.DefaultTreeModel(treeNode1));
-        jScrollPane1.setViewportView(schoolTree);
+        setLayout(new javax.swing.BoxLayout(this, javax.swing.BoxLayout.Y_AXIS));
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
-        this.setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 233, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 589, Short.MAX_VALUE))
+        backButton.setText("< Back");
+        backButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                backButtonActionPerformed(evt);
+            }
+        });
+
+        jLabel1.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel1.setText("Classrooms");
+
+        classroomsTable.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "ID", "Age range", "Group size", "Max groups allowed"
+            }
+        ) {
+            Class[] types = new Class [] {
+                java.lang.Integer.class, java.lang.String.class, java.lang.Integer.class, java.lang.Integer.class
+            };
+            boolean[] canEdit = new boolean [] {
+                false, false, false, false
+            };
+
+            public Class getColumnClass(int columnIndex) {
+                return types [columnIndex];
+            }
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
+            }
+        });
+        jScrollPane1.setViewportView(classroomsTable);
+
+        viewDetailsButton.setText("View details");
+        viewDetailsButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                viewDetailsButtonActionPerformed(evt);
+            }
+        });
+        jPanel1.add(viewDetailsButton);
+
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addGap(62, 62, 62)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 765, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addComponent(backButton)
+                        .addGap(134, 134, 134)
+                        .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 380, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
+                .addGap(0, 3, Short.MAX_VALUE)
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, 900, javax.swing.GroupLayout.PREFERRED_SIZE))
         );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 480, Short.MAX_VALUE)
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addGap(22, 22, 22)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 46, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(backButton))
+                .addGap(18, 18, 18)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 312, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, 34, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(81, Short.MAX_VALUE))
         );
+
+        add(jPanel2);
     }// </editor-fold>//GEN-END:initComponents
+
+    private void backButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_backButtonActionPerformed
+        // TODO add your handling code here:
+        container.remove(this);
+        CardLayout layout = (CardLayout) container.getLayout();
+        layout.previous(container);
+    }//GEN-LAST:event_backButtonActionPerformed
+
+    private void viewDetailsButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_viewDetailsButtonActionPerformed
+        // TODO add your handling code here:
+        int selectedRow = classroomsTable.getSelectedRow();
+
+        if (selectedRow < 0) {
+            return;
+        }
+
+        // Get the teacherId of the selected row from the table
+        int teacherId = (int) classroomsTable.getValueAt(selectedRow, 0);
+
+        // AbstractPerson selectedTeacher = school.findTeacherById(teacherId);
+        for (Classroom classroom : school.getClassroomList()) {
+            List<Teacher> teachers = classroom.getTeacherList();
+            for (Teacher teacher : teachers) {
+                if (teacher.getId() == teacherId) {
+                    TeacherInformationPanel teacherInfoPanel = new TeacherInformationPanel(container, teacher);
+                    container.add(teacherInfoPanel);
+                    CardLayout layout = (CardLayout) container.getLayout();
+                    layout.next(container);
+                    return;
+                }
+            }
+        }
+        System.out.println("Teacher not found");
+        // AbstractPerson selectedStudent = (AbstractPerson) studentsTable.getValueAt(selectedRow, 4);
+    }//GEN-LAST:event_viewDetailsButtonActionPerformed
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton backButton;
+    private javax.swing.JTable classroomsTable;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JTree schoolTree;
+    private javax.swing.JButton viewDetailsButton;
     // End of variables declaration//GEN-END:variables
 }
